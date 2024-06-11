4543c1ff99b03e7dc55efb40c90eaaba3b90b0f9
frankhart2018 <sdhargithub@gmail.com>
2024-06-10 23:02:44 -0500

    Add user question state
    Add user question state

--- persona_sync_pylib/types/chat_agents/user_questions.py
+++ persona_sync_pylib/types/chat_agents/user_questions.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from enum import Enum
+
+
+class UserQuestionState(Enum):
+    PENDING = "PENDING"
+    ANSWERED = "ANSWERED"
+    REJECTED = "REJECTED"
 
 
 class UserQuestion(BaseModel):
@@ -8,4 +15,8 @@ class UserQuestion(BaseModel):
     question_embedding: List[float]
     question_id: str
     interaction_id: str
+    state: Optional[UserQuestionState] = UserQuestionState.PENDING
     answer: Optional[str] = ""
+
+    class Config:
+        use_enum_values = True
